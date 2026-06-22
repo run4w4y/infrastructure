9b4be7df566f98ca79d84af3c7ca5f267faaa0cf
Vadim Makarov <add4che@gmail.com>
2026-06-22 23:36:04 +0300

    use a shared nix derivation for infisical cli, instead of defining a local one

--- flake.nix
+++ flake.nix
@@ -4,19 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    infisical-nix = {
+      url = "git+ssh://git@github.com/run4w4y/infisical-nix.git";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (
+  outputs = { self, nixpkgs, flake-utils, infisical-nix }: flake-utils.lib.eachDefaultSystem (
     system:
     let
       pkgs = import nixpkgs {
         inherit system;
         config.allowUnfree = true;
       };
-      infisical-cli = import ./infisical.nix
-        {
-          inherit pkgs system;
-        };
+      infisical-cli = infisical-nix.packages.${system}.infisical-cli;
     in
     with pkgs;
     {
